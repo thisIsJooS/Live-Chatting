b761f5e763fa5bd0827530a43eec1cb6da12a2c8
thisIsJooS <thisis.joos@gmail.com>
2022-06-02 15:10:10 +0900

    소켓 관련 오류들 수정

--- app.js
+++ app.js
@@ -34,8 +34,9 @@ const sessionMiddleware = session({
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/gif", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(sessionMiddleware);
 app.use((req, res, next) => {
